57c296bd2b800817ad112c5ecde25afbc1d57b46
Diser12 <ejdissinger12@gmail.com>
2014-04-23 02:48:08 -0400

    Update attenuator.c

--- src/attenuator.c
+++ src/attenuator.c
@@ -1,11 +1,21 @@
 #include <stdlib.h>
 #include "attenuator.h"
 
+typedef struct {
+  float attack;
+  float decay;
+} At;
+
 /*
  * attack: how gradually the gain will increase, if the gain changes
  * decay: how gradually gain will decrease
  */
-//void* at_new(float attack, float decay);
+void* at_new(float attack, float decay) {
+  At* at = malloc(sizeof(At));
+  at->attack = attack;
+  at->decay = decay;
+  return (void*)at;
+}
 
 /*
  * attentuates gradually, from start of the buffer to 
@@ -13,8 +23,7 @@
  * adjustments from attack/decay
  */
 float at_filter(float input, int i, int buflength, void* data) {
-  (void)data;
-  (void)i;
+  At* at = (At*)data;
   
   for (int j = 0; j < buflength; j++) {
     printf("Looping\n");
@@ -23,7 +32,10 @@ float at_filter(float input, int i, int buflength, void* data) {
   
 }
 
-//void at_free(void* data);
+void at_free(void* data) {
+  At* at = (At*)data;
+  free(at);
+}
 
 /* changes the gain of the attenuator */
 //void at_gain(float gain, void* data);
