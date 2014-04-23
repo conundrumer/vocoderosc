23d0c93bc2ae4e9f2f29a9cdabe52cfe43217fc0
David Lu <conundrumer@gmail.com>
2014-04-23 19:56:59 -0400

    vocoder header

---
+++ src/vocoder.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "fx.h"
+
+typedef struct {
+	Fx* vcm;
+	Fx* vcc;
+} Vocoder;
+
+Vocoder* vc_new(float f_low, float f_high, int numBands, int fs);
+
+float vc_process(float input_m, float input_c, int i, int bufLength, Vocoder* vc);
+
+void vc_free(Vocoder* vc);
