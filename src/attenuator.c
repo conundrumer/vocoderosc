8812d95bdc83eb340da73313d847678b8ab9730c
Diser12 <ejdissinger12@gmail.com>
2014-04-22 23:58:08 -0400

    Update attenuator.c

--- src/attenuator.c
+++ src/attenuator.c
@@ -5,7 +5,7 @@
  * attack: how gradually the gain will increase, if the gain changes
  * decay: how gradually gain will decrease
  */
-void* at_new(float attack, float decay);
+//void* at_new(float attack, float decay);
 
 /*
  * attentuates gradually, from start of the buffer to 
@@ -13,12 +13,14 @@ void* at_new(float attack, float decay);
  * adjustments from attack/decay
  */
 float at_filter(float input, int i, int buflength, void* data) {
-  // LOL WUT
-  return 1.0;
+  for (int j = 0; j < buflength; j++) {
+    
+  }
+  return input;
   
 }
 
-void at_free(void* data);
+//void at_free(void* data);
 
 /* changes the gain of the attenuator */
-void at_gain(float gain, void* data);
+//void at_gain(float gain, void* data);
