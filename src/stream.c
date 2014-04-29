dfc705b64f18ed23702060016b23b926f160b413
njung92 <njjung@cmu.edu>
2014-04-28 22:44:34 -0400

    reverting src/stream.c

--- src/stream.c
+++ src/stream.c
@@ -130,7 +130,7 @@ error:
     fprintf( stderr, "An error occured while using the portaudio stream\n" );
     fprintf( stderr, "Error number: %d\n", err );
     fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
-    synth_free(synth);
+    vc_free(vc);
     return err;
 }
 
